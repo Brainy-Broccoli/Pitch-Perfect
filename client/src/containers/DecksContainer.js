a9357f8c7e13a419acebb92cff3a1e0044238601
Yermek Alzanov <yermek-alzhanov@utulsa.edu>
2017-07-18 14:04:17 -0700

    add create custom deck icon

--- client/src/containers/DecksContainer.js
+++ client/src/containers/DecksContainer.js
@@ -16,32 +16,20 @@ import { connect } from 'react-redux';
 import { Grid, Image } from 'semantic-ui-react';
 import Deck from '../components/Deck';
 
-
-var DummyData = [
-  {id:1, topicName: 'Topic Name', image: 'https://static.pexels.com/photos/20974/pexels-photo.jpg'},
-  {id:2, topicName: 'Topic Name', image: 'https://static.pexels.com/photos/20974/pexels-photo.jpg'},
-  {id:3, topicName: 'Topic Name', image: 'https://static.pexels.com/photos/20974/pexels-photo.jpg'},
-  {id:4, topicName: 'Topic Name', image: 'https://static.pexels.com/photos/20974/pexels-photo.jpg'},
-  {id:5, topicName: 'Topic Name', image: 'https://static.pexels.com/photos/20974/pexels-photo.jpg'},
-  {id:6, topicName: 'Topic Name', image: 'https://static.pexels.com/photos/20974/pexels-photo.jpg'},
-  {id:7, topicName: 'Topic Name', image: 'https://static.pexels.com/photos/20974/pexels-photo.jpg'},
-  {id:8, topicName: 'Topic Name', image: 'https://static.pexels.com/photos/20974/pexels-photo.jpg'}
-];
-
-class DecksContainer extends Component {
-  render() {
-    return (
-      <Grid>
-        <Grid.Row>
-          {
-            this.props.decksInfo.decks.map((deck) => {
-              return <Deck topic={deck.topicName} image={deck.image} key={deck.id}/>
-            })
-          }
-        </Grid.Row>
-      </Grid>
-    );
-  }
+const DecksContainer = (props) => {
+  var image = 'http://www.freeiconspng.com/uploads/grey-plus-icon-8.png';
+  return (
+    <Grid>
+      <Grid.Row>
+        {
+          props.decksInfo.decks.map((deck) => {
+            return <Deck topic={deck.topicName} image={deck.image} key={deck.id}/>
+          })
+        }
+        <Deck topic={'Create custom deck'} image={image} key={'customDeck'}/>
+      </Grid.Row>
+    </Grid>
+  );
 };
 
 const mapStateToProps = (state) => {
