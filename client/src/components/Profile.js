1e6c0673072123e400c419971fc2e866dd443497
Michael Chiang <chiangshtaa@gmail.com>
2017-07-17 20:04:24 -0700

    include dummy data for profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -9,25 +9,49 @@ class Profile extends Component {
       <Grid>
         <Grid.Column width={6}>
           <Image style={{height: 241, width: 400}} src={this.props.profileInfo.photo}/>
-          {this.props.profileInfo.name}
+          <div style={{textAlign: 'center', marginTop: 15}}>
+            <h2>{this.props.profileInfo.name}</h2>
+            <div style={{textAlign: 'center'}}>
+              <h3 style={{margin: 0}}> Badges Earned</h3>
+              {this.props.profileInfo.badges.map((badge, index) => {
+                return (<img key={index} style={{display: 'inline-block', height: 40, width: 40}} src={badge} />);
+              })}
+            </div>
+          </div>
           <div>
           </div>
         </Grid.Column>
-        <Grid.Column width={9}>
-          <div>
-            {this.props.profileInfo.mentor.illegible ? <img style={{height: 40, width: 40}} src={this.props.profileInfo.mentor.icon}/> : null}
+        <Grid.Column width={10}>
+          <div style={{textAlign: 'center'}}>
+            <h2 style={{display: 'inline-block'}}> Recent Activity </h2>
+            {this.props.profileInfo.mentor.eligible ? <img style={{float: 'right', height: 40, width: 40}} src={this.props.profileInfo.mentor.icon}/> : null}
           </div>
-          {this.props.profileInfo.badges.map((badge, index) => {
-            return (<img key={index} style={{display: 'inline-block', height: 25, width: 25}} src={badge} />);
-          })}
+          <hr/>
           <div>
-            {this.props.profileInfo.recent.map((recentDeck, idx) => (<div key={idx}>lmao</div>) )}
+            {this.props.profileInfo.recent.map((recentDeck, idx) => {
+              return (
+                <Grid key={idx}>
+                  <Grid.Column width={7}>
+                    <img src={recentDeck.image} style={{height: 200, width: 180, borderStyle: 'groove', borderWidth: 7, borderColor: '#e00000', borderRadius: 10}}/>
+                  </Grid.Column>
+                  <Grid.Column width={8}>
+                    <div style={{fontSize: 28}}><b>{recentDeck.deck}</b></div>
+                    <div style={{fontSize: 18, marginTop: 44}}>
+                      <div><b>Accuracy: </b>{recentDeck.accuracy}</div>
+                      <div><b>Progress: </b>{recentDeck.progress} / {recentDeck.total}</div>
+                      <div><b>Badge: </b><img src={recentDeck.badge} style={{height: 40, width: 40}}/></div>
+                    </div>
+                  </Grid.Column>
+                </Grid>
+              )
+            })}
           </div>
         </Grid.Column>
       </Grid>
     );
   }
 }
+
 
 const mapStateToProps = (state) => {
   // Whatever is returned will show up as props 
@@ -37,3 +61,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(Profile);
+
+
