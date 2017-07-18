ec32d4a70d61226d389fb283d64fd55bb8741fd6
Mason Raasch <raasch.mason@gmail.com>
2017-07-18 14:50:45 -0700

    redo grid styling for practice page and add dropdown menu to select a card based off the current deck

--- client/src/components/PracticePage.js
+++ client/src/components/PracticePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
-
+import { Grid, Header, Divider } from 'semantic-ui-react';
+import QuickSelectDropdown from './QuickSelectDropdown';
 
 
 
@@ -9,13 +9,27 @@ class PracticePage extends Component {
     super(props);
   }
 
-  render() {
+  render() {  
     return (
-      <div>
-      
-        <Header as="h1">{this.props.currentDeck.topic}</Header>
-        <Header as="h2">{this.props.currentCard.positionInDeck}/{this.props.currentDeck.cards.length}</Header>
-      </div>
+      <Grid textAlign='center' columns={3} divided>
+        <Grid.Row>
+          <Grid.Column> 
+            <Header as='h1'>{this.props.currentCard.character}</Header>
+            <Header as='h2'>IPA/Pinyin: {this.props.currentCard.IPA}/{this.props.currentCard.pinyin}</Header>
+            <Header as='h2'>{this.props.currentCard.translation}</Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Header as='h1'>{this.props.currentDeck.topic}</Header>
+            <Header as='h2'>{this.props.currentCard.positionInDeck}/{this.props.currentDeck.cards.length}</Header>
+          </Grid.Column>
+          <Grid.Column>
+            <QuickSelectDropdown 
+              deck={this.props.currentDeck} 
+              currentCard={this.props.currentCard}
+            />
+          </Grid.Column>  
+        </Grid.Row>
+      </Grid>
     );
   }
 }
---
+++ client/src/components/QuickSelectDropdown.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Dropdown } from 'semantic-ui-react';
+
+const QuickSelectDropdown = (props) => {
+  const options = props.deck.cards.map(card => {
+    return {
+      key: card.positionInDeck,
+      text: `${card.character} | ${card.translation}`,
+      value: card.positionInDeck
+    };
+  });
+  return (
+    <Dropdown
+      selection
+      options={options}
+      placeholder='Pick a card'
+    />
+  );
+};
+
+export default QuickSelectDropdown;
