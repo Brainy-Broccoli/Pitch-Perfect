15568d48cc57f016dec5ea0558088e738b4fa9b2
Michael Chiang <chiangshtaa@gmail.com>
2017-07-17 19:05:19 -0700

    implement active status page

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import { 
   BrowserRouter as Router,
   Route,
@@ -11,11 +14,9 @@ import Decks from './Decks';
 import PremiumContent from './PremiumContent';
 import Logout from './Logout';
 
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
 import { Menu, Segment } from 'semantic-ui-react';
 
+import { selectPage } from '../actions/action_selectPage';
 // import Decks
 // import Logout Page
 // likely additional routes will need to be defined within the decks page 
@@ -26,26 +27,37 @@ class App extends Component {
       <Router>
         <div>
           <Menu pointing secondary>
-            <Menu.Item name='home' as={Link} to='/' /> 
-            <Menu.Item name='decks' as={Link} to='/decks' /> 
-            <Menu.Item name='Premium' as={Link} to='/premium-content' /> 
+            <Menu.Item name='home' as={Link} to='/' active={this.props.activePage === 'home'} onClick={(event, itemProps) => this.props.selectPage(itemProps.name)}/> 
+            <Menu.Item name='decks' as={Link} to='/decks' active={this.props.activePage === 'decks'} onClick={(event, itemProps) => this.props.selectPage(itemProps.name)}/> 
+            <Menu.Item name='Premium' as={Link} to='/premium-content' active={this.props.activePage === 'Premium'} onClick={(event, itemProps) => this.props.selectPage(itemProps.name)}/> 
             <Menu.Menu position='right'>
-              <Menu.Item name='logout' as={Link} to='/logout' /> 
+              <Menu.Item name='logout' as={Link} to='/logout' active={this.props.activePage === 'logout'} onClick={(event, itemProps) => this.props.selectPage(itemProps.name)}/> 
             </Menu.Menu>
           </Menu>
-        
-          <hr/>
 
-          <Route exact path="/" component={Profile}/>
-          <Route path="/decks" component={Decks}/>
-          <Route path="/premium-content" component={PremiumContent}/>
-          <Route path="/logout" component={Logout}/>
+          <Segment>
+            <Route exact path="/" component={Profile}/>
+            <Route path="/decks" component={Decks}/>
+            <Route path="/premium-content" component={PremiumContent}/>
+            <Route path="/logout" component={Logout}/>
+          </Segment>
         </div>
       </Router>
     );
   }
 };
 
-export default App;
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props 
+  return {
+    activePage: state.activePage
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectPage: selectPage }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 
