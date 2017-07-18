97a455c4d06e0abf98f1fcaa5da6fbd676e9cb4c
Michael Chiang <chiangshtaa@gmail.com>
2017-07-18 16:17:32 -0700

    render profile page with style and sample data

--- client/src/reducers/reducer_profile.js
+++ client/src/reducers/reducer_profile.js
@@ -4,29 +4,35 @@
 const initialState = {
   name: 'Michael',
   photo: 'https://www.cbdeolali.org.in/drupal/sites/default/files/Section%20Head/Alternative-Profile-pic_5.jpg',
-  badges: ['https://previews.123rf.com/images/viktorijareut/viktorijareut1508/viktorijareut150800476/44097508-Vector-illustration-of-golden-trophy-cup-for-first-place-with-laurel-wreath-Trophy-icon-Sport-award--Stock-Vector.jpg', 'https://previews.123rf.com/images/viktorijareut/viktorijareut1508/viktorijareut150800476/44097508-Vector-illustration-of-golden-trophy-cup-for-first-place-with-laurel-wreath-Trophy-icon-Sport-award--Stock-Vector.jpg', 'https://previews.123rf.com/images/viktorijareut/viktorijareut1508/viktorijareut150800476/44097508-Vector-illustration-of-golden-trophy-cup-for-first-place-with-laurel-wreath-Trophy-icon-Sport-award--Stock-Vector.jpg'],
+  badges: ['https://proprofs-cdn.s3.amazonaws.com/images/games/user_images/misc/5211927825.png', 'https://previews.123rf.com/images/viktorijareut/viktorijareut1508/viktorijareut150800476/44097508-Vector-illustration-of-golden-trophy-cup-for-first-place-with-laurel-wreath-Trophy-icon-Sport-award--Stock-Vector.jpg', 'http://www.freeiconspng.com/uploads/travel-3-icon--colorflow-iconset--tribalmarkings-24.png', 'http://icons.iconarchive.com/icons/bokehlicia/pacifica/256/weather-icon.png'],
   recent: [
     {
-      deck: 'food',
+      deck: 'Food',
+      image: 'http://worldartsme.com/images/chinese-restaurant-clipart-1.jpg',
+      badge: 'https://proprofs-cdn.s3.amazonaws.com/images/games/user_images/misc/5211927825.png',
       progress: 6,
       total: 13,
       accuracy: 89 + '%'
     },
     {
-      deck: 'travel',
+      deck: 'Travel',
+      image: 'https://iabuk.net/sites/default/files/styles/thumbnail/public/shutterstock_58918264.jpg?itok=sYn7AJu0',
+      badge: 'http://www.freeiconspng.com/uploads/travel-3-icon--colorflow-iconset--tribalmarkings-24.png',
       progress: 8,
       total: 19,
       accuracy: 32 + '%'
     },
     {
-      deck: 'food',
+      deck: 'Weather',
+      image: 'https://icons.wxug.com/i/c/v4/partlycloudy.svg',
+      badge: 'http://icons.iconarchive.com/icons/bokehlicia/pacifica/256/weather-icon.png',
       progress: 7,
       total: 18,
       accuracy: 55 + '%'
     }
   ],
   mentor: {
-    elegible: true,
+    eligible: true,
     icon: 'https://static3.bigstockphoto.com/4/8/9/small2/98422448.jpg'
   }
 };
