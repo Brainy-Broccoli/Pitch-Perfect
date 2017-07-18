8ab9ffd86008d420cf2ecef1d495b0fe9f5f478c
Mason Raasch <raasch.mason@gmail.com>
2017-07-18 12:16:30 -0700

    add initial state data for currentDeck and currentCard

--- client/src/reducers/reducer_currentCard.js
+++ client/src/reducers/reducer_currentCard.js
@@ -1,6 +1,20 @@
-const currentCard = (state = {}, action) => {
+const cardMom = {
+  character: '妈妈',
+  IPA: 'mama',
+  pinyin: 'mama',
+  soundUrl: 'soundcloud.com',
+  translation: 'mom',
+  userAccuracy: 85,
+  positionInDeck: 1 
+};
+
+const initialState = cardMom;
+
+const currentCard = (state = initialState, action) => {
   // action -- updating the card info
   switch (action.type) {
+    case 'DROPDOWN_CARD_SELECTED':
+      return action.newCard;
     default:
       return state;
   }
--- client/src/reducers/reducer_currentDeck.js
+++ client/src/reducers/reducer_currentDeck.js
@@ -12,12 +12,43 @@
 
 // export default reducerFunctionReference
 
-const deck = (state = [], action) => {
+// a deck will be an array of card objects
+
+const cardMom = {
+  character: '妈妈',
+  IPA: 'mama',
+  pinyin: 'mama',
+  translation: 'mom',
+  userAccuracy: 85,
+  positionInDeck: 1 
+};
+
+const cardDog = {
+  character: '狗',
+  IPA: 'gou',
+  pinyin: 'gou',
+  translation: 'dog',
+  userAccuracy: 49,
+  positionInDeck: 2 
+};
+
+const cardTall = {
+  character: '高',
+  IPA: 'gao',
+  pinyin: 'gao',
+  translation: 'tall',
+  userAccuracy: 131,
+  positionInDeck: 3 
+};
+
+const initialState = [cardMom, cardDog, cardTall];
+
+const currentDeck = (state = initialState, action) => {
   // actions -- changing the deck
   switch (action.type) {
     default:
-      return state; 
+      return state;
   }
 };
 
-export default deck;
+export default currentDeck;
