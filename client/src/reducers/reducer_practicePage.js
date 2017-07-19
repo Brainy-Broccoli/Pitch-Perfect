7c8fd65f2c7f87625b4c63fd0642069d1c050334
Mason Raasch <raasch.mason@gmail.com>
2017-07-19 11:14:00 -0700

    delete unnecessary example file. practice page reducer can now handle 2 more card selection events

--- client/src/reducers/reducer_practicePage.js
+++ client/src/reducers/reducer_practicePage.js
@@ -84,13 +84,11 @@ const practicePage = (state = initialState, action) => {
         currentCard: state.currentDeck.cards[action.cardPos]
       };
     case 'SELECT_PREVIOUS_CARD':
-      console.log('the previous card index', state.currentCard.positionInDeck - 1);
       return {
         currentDeck: state.currentDeck,
         currentCard: state.currentDeck.cards[state.currentCard.positionInDeck - 1] ? state.currentDeck.cards[state.currentCard.positionInDeck - 1] : state.currentCard
       };
     case 'SELECT_NEXT_CARD':
-      console.log('the next card', state.currentDeck.cards[state.currentCard.positionInDeck + 1]);
       return {
         currentDeck: state.currentDeck,
         currentCard: state.currentDeck.cards[state.currentCard.positionInDeck + 1] ? state.currentDeck.cards[state.currentCard.positionInDeck + 1] : state.currentCard
