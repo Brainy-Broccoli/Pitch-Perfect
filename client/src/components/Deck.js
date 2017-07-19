de51a501e416c4ac0c5508f321178808283fdc27
Michael Chiang <chiangshtaa@gmail.com>
2017-07-19 10:13:48 -0700

    nav bar update to practice page on click of deck

--- client/src/components/Deck.js
+++ client/src/components/Deck.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, Grid, Image } from 'semantic-ui-react'
 
+import { bindActionCreators } from 'redux';
+import { selectPage } from '../actions/actions_navBar';
+
 // const Deck = () => {
-class Deck extends React.Component {
-  constructor (props) {
-    super(props);
-  }
+class Deck extends Component {
+  // constructor (props) {
+  //   super(props);
+  // }
   render() {
     return (
       <Grid.Column width={5}>
@@ -15,7 +18,8 @@ class Deck extends React.Component {
             <strong>{this.props.topic}</strong>
           </div>
           <div>
-            <Image src={this.props.image} style={{height: 200, width: 400}} />
+            <Image src={this.props.image} style={{height: 200, width: 400}}
+              onClick={() => this.props.selectPage('Practice Page')} />
           </div>
         </Card>
       </Grid.Column>
@@ -23,5 +27,10 @@ class Deck extends React.Component {
   }
 };
 
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ selectPage: selectPage }, dispatch);
+};
 
-export default Deck;
+export default connect(null, mapDispatchToProps)(Deck);
+
+// export default Deck;
