e5a4115111b9ba6602875355724e1a6844cf8d91
Mason Raasch <raasch.mason@gmail.com>
2017-07-21 18:59:01 -0700

    update the exports object of the db so that knex is easily accessible. update server route object so we have a route to fetch data from

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const knex = require('../../db').knex;
 
 router.route('/')
   .get((req, res) => {
@@ -9,6 +10,19 @@ router.route('/')
   .post((req, res) => {
     console.log('in the correct route');
     res.status(201).send({ data: 'Posted!' });
+  });
+
+router.route('/profileInfo')
+  .get((req, res) => {
+    const userInfo = req.user;
+    console.log('user info', userInfo);
+
+    knex.select().table('profiles')
+      .then(rows => {
+        console.log(rows);
+        res.status(200).json(rows);  
+      })
+      .catch(err => res.status(500).send(err));
   });
 
 module.exports = router;
