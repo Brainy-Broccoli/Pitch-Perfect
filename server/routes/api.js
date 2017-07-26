267815c763988ab5cb6d08e4357daa9803bf1ffc
JD Broberg <jdbroberg@yahoo.com>
2017-07-25 22:46:33 -0700

    users_decks updates after updating card

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,17 +7,6 @@ const knex = require('../../db').knex;
 router.route('/profile')
   .get(middleware.auth.verify, (req, res) => {
     const userID = req.user.id;
-    const name = `${req.user.first} ${req.user.last}`;
-    const badges = [];
-    const photo = req.user.photo;
-    const profilePageData = {
-      name,
-      badges,
-      photo
-    };
-
-    const allDecks = [];
-
     knex.from('users_decks')
       .innerJoin('profiles', 'users_decks.user_id', '=', 'profiles.id')
       .innerJoin('decks', 'users_decks.deck_id', '=', 'decks.id')
@@ -39,7 +28,6 @@ router.route('/profile')
             .innerJoin('cards', 'decks_cards.card_id', '=', 'cards.id')
             .where({deck_id: deck.id, user_id: userID})
             .then( cards => {
-              console.log(cards.length)
               const destructuredCards = cards.map( card => ({
                 id: card.card_id,
                 character: card.character,
@@ -68,14 +56,54 @@ router.route('/profile')
 
 router.route('/card/:id')
   .post(middleware.auth.verify, (req, res) => {
-    knex.from('users_cards')
-      .innerJoin('cards','users_cards.card_id', '=', 'cards.id')
-      .where({ card_id: req.params.id, user_id: req.user.id })
-      .then( resp => {
-      console.log(req.body)
-      console.log(resp)
-      res.send('lol')
-      }).catch( err=> console.error(err));
+    const userID = req.user.id;
+    const cardID = req.params.id;
+    knex.select('high_score').from('users_cards')
+      .where({ card_id: cardID, user_id: userID })
+      .then( hs => knex.from('users_cards')
+        .where({ card_id: cardID, user_id: userID })
+        // Plz don't hack
+        .update({high_score: Math.min(Math.max(Number(hs[0]['high_score']), req.body.score, 0), 100)})
+      ).then( () => knex.from('users_decks')
+        .where({user_id: userID})
+      ).then( usersDecks => Promise.all(
+        usersDecks.map(deck => 
+          knex.count('users_cards.card_id')
+            .from('users_decks')
+            .innerJoin('decks_cards', 'users_decks.deck_id', '=', 'decks_cards.deck_id')
+            .innerJoin('users_cards', 'decks_cards.card_id', '=', 'users_cards.card_id')
+            .where('high_score', '>=', '80')
+            .andWhere({ 
+              'users_decks.user_id': userID,
+              'users_cards.user_id': userID,
+              'users_decks.deck_id': deck.deck_id 
+            }).then( cardsAboveEighty =>({
+              deckID: deck.deck_id,
+              completeCount: parseInt(cardsAboveEighty[0].count)
+            })).then( deckCompletion => knex.avg('high_score')
+              .from('users_cards')
+              .innerJoin('decks_cards', 'users_cards.card_id', '=', 'decks_cards.card_id')
+              .innerJoin('users_decks', 'users_decks.deck_id', '=', 'decks_cards.deck_id')
+              .where({
+                'users_cards.user_id': userID,
+                'users_decks.user_id': userID,
+                'users_decks.deck_id': deckCompletion.deckID
+              })
+              .then( average => Object.assign(deckCompletion, { average: average[0].avg }))
+            )
+        ))).then( deckCompletions => Promise.all(deckCompletions.map( deck => 
+          console.log(deck.average)||
+          knex.from('users_decks')
+            .where({
+              user_id: userID,
+              deck_id: deck.deckID
+            })
+            .update({
+              deck_progress: deck.completeCount,
+              accuracy: deck.average
+            })
+        )))
+      .then(()=>res.send('ok')).catch( err=> console.error(err) || res.status(500).send(err));
   })
 
 module.exports = router;
