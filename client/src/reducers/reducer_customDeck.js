bb7031a4c9b795819b4e0a7bb230c5d331ce32a0
Yermek Alzanov <yermek-alzhanov@utulsa.edu>
2017-07-25 19:59:20 -0700

    created layout for custom deck page

---
+++ client/src/reducers/reducer_customDeck.js
@@ -0,0 +1,10 @@
+// const loadCards = (state = [], action) => {
+//   switch (action.type) {
+//   case 'CARDS_LOADED':
+//     return action.allCards;
+//   default:
+//     return state;
+//   }
+// };
+
+// export default loadCards;
