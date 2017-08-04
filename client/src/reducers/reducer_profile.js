acf985b93612fd4b7aa3e970076e7ff6fa0cfdfa
Michael Chiang <chiangshtaa@gmail.com>
2017-08-03 20:03:09 -0700

    initialize mentorship status as false

--- client/src/reducers/reducer_profile.js
+++ client/src/reducers/reducer_profile.js
@@ -3,10 +3,10 @@
 import { LOAD_PROFILE } from '../actions/actionTypes';
 
 const initialState = {
-  name: 'Michael Chiang',
+  name: '',
   photo: 'https://www.cbdeolali.org.in/drupal/sites/default/files/Section%20Head/Alternative-Profile-pic_5.jpg',
-  badgeUrls: ['https://proprofs-cdn.s3.amazonaws.com/images/games/user_images/misc/5211927825.png', 'https://previews.123rf.com/images/viktorijareut/viktorijareut1508/viktorijareut150800476/44097508-Vector-illustration-of-golden-trophy-cup-for-first-place-with-laurel-wreath-Trophy-icon-Sport-award--Stock-Vector.jpg', 'http://www.freeiconspng.com/uploads/travel-3-icon--colorflow-iconset--tribalmarkings-24.png', 'http://icons.iconarchive.com/icons/bokehlicia/pacifica/256/weather-icon.png'],
-  isMentor: true,
+  badgeUrls: [],
+  isMentor: false,
 };
 
 const profileInfo = (state = initialState, action) => {
